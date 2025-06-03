39c5a26c6d496d1754f30f263a026b0915ddab70
huuyii <3134846106@qq.com>
2025-06-03 08:55:35 +0800

    提交

---
+++ SKU/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import sku from './SKU/sku.vue';
+const handleSkuInfo = (skuInfo) => {
+  console.log(skuInfo)
+}
+</script>
+
+<template>
+<sku :skuData="skuData" @sendSkuInfo = 'handleSkuInfo'/>
+</template>
+
+<style>
+
+</style>
